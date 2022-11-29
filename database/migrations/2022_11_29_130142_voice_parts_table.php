6a3553f82beb9decff1f282115d5883d54384f95
Aiden Deighton <73116721+AIDDEI@users.noreply.github.com>
2022-11-29 14:11:16 +0100

    Nieuwe tabel voice_parts

---
+++ database/migrations/2022_11_29_130142_voice_parts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voice_parts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('song_id')->constrained('songs');
+            $table->string('title');
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('voice_parts');
+    }
+};
